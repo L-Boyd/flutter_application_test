1e3bd9ff0e8292cb802793e82a6689a0ac1b0b1e
lby-magicbook <1508201988@qq.com>
2026-01-09 12:03:01 +0800

    自定义无状态组件

---
+++ lib/StatelessWidgetTest.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // MaterialApp 是一个方便的 Widget，它封装了应用程序实现 Material Design 所需的许多组件。
+  runApp(MainPageStateless());
+}
+
+class MainPageStateless extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "无状态组件",
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("无状态组件头部"),
+        ),
+        body: Center(
+          child: Text("这是一个无状态组件的示例"),
+        ),
+        bottomNavigationBar: Container(
+          height: 50,
+          child: Center(
+            child: Text("无状态组件底部"),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
